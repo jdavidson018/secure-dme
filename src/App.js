82a9616162f49b7897ab4092656dfc40daef2b4f
jdavidson018 <jdavidson018@gmail.com>
2020-09-20 22:08:18 -0500

    Added dashboard template

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
 import React from "react";
 import "./App.css";
-import Lottie from "react-lottie";
-import animationData from "./lotties/sailing-ship";
-import verifyDocumentData from "./lotties/document-verification";
-import scanDocumentData from "./lotties/scan-document";
-import sendDocumentData from "./lotties/send-document";
+import Dashboard from "./routes/Dashboard";
 
 function App() {
-  const shipOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const verifyOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: verifyDocumentData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const scanOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: scanDocumentData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const sendOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: sendDocumentData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
 
   return (
     <div>
-      <h1>Welcome to Secure DME. Here is what we can do:</h1>
-      <div>
-        <h2>Upload patient records and medical equipment information.</h2>
-        <Lottie options={scanOptions} height={400} width={400} />
-      </div>
-      <div>
-        <h2>Verify and format the uploaded information.</h2>
-        <Lottie options={verifyOptions} height={400} width={400} />
-      </div>
-      <div>
-        <h3>Securely transfer the information to your DME provider for billing.</h3>
-        <Lottie options={sendOptions} height={200} width={400} />
-      </div>
+      <Dashboard/>
     </div>
   );
 }
---
+++ src/routes/Dashboard.js
@@ -0,0 +1,286 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Link from "@material-ui/core/Link";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import { mainListItems, secondaryListItems } from "../components/listItems";
+import Chart from "../components/Chart";
+import Deposits from "../components/Deposits";
+import Orders from "../components/Orders";
+import Lottie from "react-lottie";
+import animationData from "../lotties/sailing-ship";
+import verifyDocumentData from "../lotties/document-verification";
+import scanDocumentData from "../lotties/scan-document";
+import sendDocumentData from "../lotties/send-document";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://github.com/jdavidson018">
+        Badson Software Solutions
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const drawerWidth = 240;
+
+const verifyOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: verifyDocumentData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+const scanOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: scanDocumentData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+const sendOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: sendDocumentData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+const shipOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: "none",
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
+
+export default function Dashboard() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            SecureDME
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item lg={12}>
+              <Paper>
+                <Typography component="h1" variant="h4" align="center">
+                  SecureDME can help you right the ship for you Durable Medical Equipment Inventory and Billing.
+                </Typography>
+                <Lottie options={shipOptions} height={200} width={200} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <Paper>
+                <Typography component="h1" variant="h6" align="center">
+                  Upload patient records and medical equipment information.
+                </Typography>
+                <Lottie options={scanOptions} height={200} width={200} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <Paper>
+                <Typography component="h1" variant="h6" align="center">
+                  Verify and format the uploaded information.
+                </Typography>
+                <Lottie options={verifyOptions} height={200} width={200} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <Paper>
+                <Typography component="h1" variant="h6" align="center">
+                  Securely transfer the information to your DME provider for
+                  billing.
+                </Typography>
+                <Lottie options={sendOptions} height={200} width={400} />
+              </Paper>
+            </Grid>
+            {/* Chart */}
+            {/* <Grid item xs={12} md={8} lg={9}>
+              <Paper className={fixedHeightPaper}>
+                <Chart />
+              </Paper>
+            </Grid> */}
+            {/* Recent Deposits */}
+            {/* <Grid item xs={12} md={4} lg={3}>
+              <Paper className={fixedHeightPaper}>
+                <Deposits />
+              </Paper>
+            </Grid> */}
+            {/* Recent Orders */}
+            {/* <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <Orders />
+              </Paper>
+            </Grid> */}
+          </Grid>
+          <Box pt={4}>
+            <Copyright />
+          </Box>
+        </Container>
+      </main>
+    </div>
+  );
+}
